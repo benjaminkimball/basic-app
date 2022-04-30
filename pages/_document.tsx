821746f74e3c5940acfbf3b0adac28d52e813e2c
Benjamin Kimball <hello@benjaminkimball.com>
2022-04-30 09:52:09 -0600

    Add Jest, style global elements

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,12 +3,12 @@ import Document, { Head, Html, Main, NextScript } from "next/document";
 class CustomDocument extends Document {
   render() {
     return (
-      <Html lang="en-US" dir="ltr">
+      <Html lang="en-US" dir="ltr" className="h-full">
         <Head>
           <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
         </Head>
 
-        <body>
+        <body className="h-full">
           <Main />
           <NextScript />
         </body>
