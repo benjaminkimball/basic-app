65f37ac823b3547de2eb3916c0b75828a65e82b1
Benjamin Kimball <hello@benjaminkimball.com>
2023-04-01 12:15:10 -0600

    Clean up tooling configurations

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,16 @@
-import { Inter } from "@next/font/google";
+import { Inter } from "next/font/google";
 import { ReactNode } from "react";
 import "../styles/globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+
+export const metadata = {
+  title: "basic-app",
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en-US" dir="ltr" className={inter.className}>
+    <html lang="en-US" dir="ltr" className={`${inter.className} min-h-screen`}>
       <body className="min-h-screen">{children}</body>
     </html>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,11 @@ const defaultTheme = require("tailwindcss/defaultTheme");
  * @type {import("tailwindcss").Config}
  */
 module.exports = {
-  content: ["app/**/*.tsx"],
+  content: ["./app/**/*.tsx"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        sans: ["var(--font-inter)", ...defaultTheme.fontFamily.sans],
       },
     },
   },
